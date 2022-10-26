aad85eb12cfdbd213b1799dce82b7730081d36d4
bouboumael <bouboumael@orange.fr>
2022-10-26 23:12:13 +0200

    maker and create table

--- config/bundles.php
+++ config/bundles.php
@@ -23,4 +23,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Translation\Bundle\TranslationBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
