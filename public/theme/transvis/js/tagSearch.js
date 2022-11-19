6861648918078137d2e61709a14b3648064c1fa0
RedPandore <houry9@gmail.com>
2022-11-19 17:13:57 +0100

    add link to filter search

--- public/theme/transvis/js/tagSearch.js
+++ public/theme/transvis/js/tagSearch.js
@@ -1,52 +1,74 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const tagList = document.getElementById('nosRealisationSection').dataset.recordsTag;
-    // json to array
-    const tagArray = JSON.parse(tagList);
-    const words = tagArray
+    // On récupère la list des tags et slug
+    const tagList = document.getElementById('buttonFilterContainer').dataset.recordsTag;
+    const taxonomy = document.getElementById('buttonFilterContainer').dataset.recordsTaxonomies;
+
+
+    
+    // On parse en JSON 
+    const wordsArr = JSON.parse(tagList);
+    const words = Object.keys(wordsArr);
+      
     const formEl = document.querySelector('#search')
     const dropEl = document.querySelector('.drop')
-    let alreadyOpen = false
+    const seeAllEl = document.querySelector('#buttonSeeAll')
+
     const formHandler = (e) => {
+        // On récupère la valeur du champ de recherche en minuscule
         const userInput = e.target.value.toLowerCase()
     
+        // Si le contenu est vidé on cache la liste
         if(userInput.length === 0) {
             dropEl.style.height = 0
             return dropEl.innerHTML = ''              
         }
     
+        // On filtre les tags en fonction de la valeur du champ de recherche
         const filteredWords = words.filter(word => word.toLowerCase().includes(userInput)).sort().splice(0, 5)
         
         dropEl.innerHTML = ''
+
+        // Pour chaque tag filtré on crée un élément li
         filteredWords.forEach(item => {
             const listEl = document.createElement('li')
             listEl.classList.add('tag-li')
-            
-            listEl.textContent = item
+            // On écoute le click sur l'élément li
+            listEl.addEventListener('click', (e) => {
+                // On récupère la valeur de l'élément li et on l'ajoute au champ de recherche
+                formEl.value = e.target.innerText
+                // On cache la liste
+                dropEl.style.height = 0
+                dropEl.innerHTML = ''
+
+                // On récupère le slug du tag
+                const slug = wordsArr[e.target.innerText]
+                var link = '/' + taxonomy + '/' + slug;
+                window.location.href = link;
+            })
+            // On ajoute le tag au contenu de l'élément li
+            listEl.textContent = item 
             dropEl.appendChild(listEl)
         })
     
+        // Si la recherche ne retourne aucun résultat on cache la liste
         if(dropEl.children[0] === undefined) {
             return dropEl.style.height = 0
         }
-    
+        
+        // On gère la hauteur de la liste en fonction du nombre d'éléments
         let totalChildrenHeight = dropEl.children[0].offsetHeight * filteredWords.length
         dropEl.style.height = totalChildrenHeight + 'px'
     
     }
- /*   
-    formEl.addEventListener('pointerdown', (e) => {
-        if(!alreadyOpen) {
-        let randomWords = words.sort(() => Math.random() - 0.5).splice(0, 2)
-        randomWords.push('Tapez votre recherche');
-        randomWords.forEach(item => {
-            const listEl = document.createElement('li')
-            listEl.classList.add('tag-li')
-            listEl.textContent = item
-            dropEl.appendChild(listEl)
-        });
-        alreadyOpen = true
+
+    const seeAllHandler = (e) => {
+        // On récupere la valeurs du data attribute
+        const seeAll = e.target.dataset.recordsAll
+        // On redirige vers la page du lien
+        window.location.href = seeAll
     }
-    })*/
 
     formEl.addEventListener('input', formHandler)
+
+    seeAllEl.addEventListener('click', seeAllHandler)
 });
