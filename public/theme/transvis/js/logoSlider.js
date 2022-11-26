ed71b3ebf194297fd1be4858624355182e5dff21
RedPandore <houry9@gmail.com>
2022-11-26 12:06:27 +0100

    nos realisations + modal single realisation

--- public/theme/transvis/js/logoSlider.js
+++ public/theme/transvis/js/logoSlider.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  var splide = new Splide(".splide", {
-    type: "loop",
-    drag: "free",
-    focus: "center",
-    perPage: 4,
-    gap: 50,
-    autoScroll: {
-      speed: 1,
-    },
-    breakpoints: {
-      1200: { perPage: 3, gap: "1rem" },
-      640: { perPage: 2, gap: 0 },
-    },
-  });
-  splide.mount(window.splide.Extensions);
-  setLogoSize();
+   var splideEl = document.querySelectorAll("#logoSlider");
+    splideEl.forEach(function (splide) {
+        var splide = new Splide(splide, {
+            type: "loop",
+            drag: "free",
+            focus: "center",
+            perPage: 4,
+            gap: 50,
+            autoScroll: {
+            speed: 1,
+            },
+            breakpoints: {
+            1200: { perPage: 3, gap: "1rem" },
+            640: { perPage: 2, gap: 0 },
+            },
+        });
+
+        splide.mount(window.splide.Extensions);
+        setLogoSize(splide);
+    });
 });
 
-function setLogoSize() {
-  var sliderWidth = document.querySelector(".splide__slide").offsetWidth;
-  var logoSize = document.querySelectorAll(".splide__slide img");
+function setLogoSize(splide) {
+  var splideDiv = splide.root;
+  var sliderWidth = splideDiv.querySelector(".splide__slide").offsetWidth;
+  var logoSize = splideDiv.querySelectorAll(".splide__slide img");
   logoSize.forEach(function (logo) {
-    logo.style.width = sliderWidth + "px";
+    logo.style.width = sliderWidth / 2 + "px";
   });
 }
