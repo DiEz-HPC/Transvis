8f5841148f9ed0e2583b3e33e1d52a359fdc9606
RedPandore <houry9@gmail.com>
2022-12-06 12:05:12 +0100

    fix admin permission

--- src/Controller/AdminCustomMenu.php
+++ src/Controller/AdminCustomMenu.php
@@ -5,29 +5,35 @@ namespace App\Controller;
 use Bolt\Menu\ExtensionBackendMenuInterface;
 use Knp\Menu\MenuItem;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-
+use Symfony\Component\Security\Core\Security;
 class AdminCustomMenu implements ExtensionBackendMenuInterface
 {
 
     public function __construct(
-        private UrlGeneratorInterface $urlGenerator
-    )
-    {
+        private UrlGeneratorInterface $urlGenerator,
+        private Security $security
+    ) {
     }
 
     public function addItems(MenuItem $menu): void
     {
-        $menu->addChild('Message de contact', [
-            'uri' => $this->urlGenerator->generate('app_contact_message'),
-            'extras' => [
-                'icon' => 'fa-envelope'
-            ]]);
-        $menu->addChild('Candidatures', [
-            'uri' => $this->urlGenerator->generate('app_candidatures'),
-            'extras' => [
-                'icon' => 'fa-file-invoice'
-            ]]);
+        // On vérifie le role de l'utilisateur
+        if ($this->security->isGranted('ROLE_PAGE_CONTACT')) {
+            $menu->addChild('Message de contact', [
+                'uri' => $this->urlGenerator->generate('app_contact_message'),
+                'extras' => [
+                    'icon' => 'fa-envelope'
+                ]
+            ]);
+        }
 
-
+        if ($this->security->isGranted('ROLE_RECRUTEMENT')) {
+            $menu->addChild('Candidatures', [
+                'uri' => $this->urlGenerator->generate('app_candidatures'),
+                'extras' => [
+                    'icon' => 'fa-file-invoice'
+                ]
+            ]);
+        }
     }
 }
