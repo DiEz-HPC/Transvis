2d4f9eb2586ccc1d4bf888deef3c9438a8842aec
RedPandore <houry9@gmail.com>
2022-12-12 17:24:25 +0100

    add select preview

--- public/theme/transvis/js/cardNosRealisations.js
+++ public/theme/transvis/js/cardNosRealisations.js
@@ -1,25 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
     var cards = document.querySelectorAll(".cardOuterContainer");
-    cards.forEach(function (card) {
-        modal = card.querySelector(".modalYoutube");
-        video = modal.querySelector("video");
-        var time = card.dataset.timerecord;
-            setTimeout(function () {
-                captureImage(video, card);
-                }, 250);
-        video.currentTime = time;
+    cards.forEach(function (card, index) {
+        var isCaptured = card.dataset.iscaptured;
+        if(isCaptured == 0 || isCaptured == '0') {
+            setTimeout(function(){
+                modal = card.querySelector(".modalYoutube");
+                video = modal.querySelector("video");
+                var time = card.dataset.timerecord;
+                var loop = card.dataset.loopindex;
+                video.addEventListener("seeked", function () {
+                    captureImage(video, card, loop)
+                }, {once : true});
+                video.currentTime = time;
+                card.dataset.iscaptured = 1;
+            }, index * 400)
+        }
     });
 });
 
 // Fonction qui permet de capturer l'image de la vidéo et de l'afficher dans la card
-let captureImage = (video, card) => {
-    let canvas = card.querySelector("#cardImage");
+var captureImage = (video, card, loop) => {
+    var canvas = card.querySelector("#cardImage"+ loop);
     if (canvas) {
-        let context = canvas.getContext("2d");
+        var context = canvas.getContext("2d");
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
         return canvas.toDataURL("image/png");
-    }else{
+    } else {
         return null;
     }
 };
-
