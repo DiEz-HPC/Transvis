140b54004bbc777c6c4ae99d5b7949b5a9accd6b
RedPandore <houry9@gmail.com>
2023-01-02 15:28:40 +0100

    set acces right

--- docker-entry.sh
+++ docker-entry.sh
@@ -2,20 +2,20 @@
 #!/bin/sh
 
 composer install
-apt update -y && apt upgrade -y && apt install npm -y && npm install -y
+apt update -yq && apt upgrade -yq && apt install npm -yq && npm install -y
 npm run build
 
-
-## Symfony configuration
-chmod -R 777 /var/www/var
-chmod -R 777 /var/www/public
-
-chmod -R 777 var
-chmod -R 777 public
 
 php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
 php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
 
+## On donne les droits sur les dossiers
+chmod -R 777 var
+chmod -R 777 public
+
+## On s'approprie les dossiers
+chown -R www-data:www-data var
+chown -R www-data:www-data public
 
 php bin/console assets:install public
 php bin/console cache:clear
