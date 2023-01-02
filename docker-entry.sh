aff11a0b8fc42051e2dbd92dad3a5d308eb3a723
RedPandore <houry9@gmail.com>
2023-01-02 15:56:58 +0100

    d

--- docker-entry.sh
+++ docker-entry.sh
@@ -9,9 +9,15 @@ npm run build
 php bin/console doctrine:database:create --if-not-exists --quiet --no-interaction
 php bin/console doctrine:migrations:migrate --verbose --no-interaction --allow-no-migration
 
+
+## On creé le dossier var/cache
+mkdir -p var/cache
+mkdir -p var/cache/prod
+
 ## On donne les droits sur les dossiers
 chmod -R 777 var
 chmod -R 777 public
+
 
 ## On attribue les droits sur les dossiers pour le user root
 chown -R root:root var
